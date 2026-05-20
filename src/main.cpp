3690ca0c2c398a7de39247ab5ae7550d2f69acc9
Omar Mohammed <omareslamg@gmail.com>
2026-05-21 00:37:30 +0300

    Added history

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,22 @@
 using namespace std;
 
 int main() {
+    vector<string> history;
     while (true) {
         char buf[256];
         cout << "o-mini-shell:" << getcwd(buf, sizeof(buf)) << "$ ";
         string cmd;
         getline(cin,cmd);
         if (cmd.empty()) continue;
+        history.push_back(cmd);
         vector<std::string> cmd_parsed = parser::parse(cmd);
         if (cmd_parsed.empty()) continue;
-        if (builtins::is_builtin(cmd_parsed[0])) builtins::handle(cmd_parsed);
+        else if(cmd_parsed[0] == "history") {
+            for (int i = 0; i < history.size(); i++) {
+                cout << history[i] << endl;
+            }
+        }
+        else if (builtins::is_builtin(cmd_parsed[0])) builtins::handle(cmd_parsed);
         else executer::execute(cmd_parsed);
     }
 }
