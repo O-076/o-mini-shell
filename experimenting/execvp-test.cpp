d5b8792e44b101096e617ce263067c880a0c9952
Omar Mohammed <omareslamg@gmail.com>
2026-05-20 17:57:52 +0300

    Trying out execvp

---
+++ experimenting/execvp-test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    cout << "Hello. I am the parent process.\n\n" << flush;
+    pid_t pid = fork();
+    if (pid == 0) {
+        cout << "This should appear.\n\n" << flush;
+        char* args [] = {"-l", NULL};
+        execvp("ls", args);
+        cout << "This shouldn't appear\n\n";
+    }
+}
